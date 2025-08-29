194b4ca029136f38e9e2c8b53cb4a213b07eabcf
Piyapat <piyapat.pai@outlook.com>
2025-08-29 20:54:19 +0700

    feat: Add Sidebar component with navigation buttons

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import styles from "../styles/Sidebar.module.css";
+
+export default function Sidebar() {
+  const [selected, setSelected] = useState("Explore");
+  return (
+    <>
+      <div className={styles.sidebar}>
+        <button
+          onClick={() => setSelected("Home")}
+          className={
+            selected === "Home"
+              ? styles.sidebarButtonSelected
+              : styles.sidebarButton
+          }
+        >
+          {selected === "Home" ? <div>iconS</div> : <div>icon</div>}
+          <div>Home</div>
+        </button>
+        <button
+          onClick={() => setSelected("Explore")}
+          className={
+            selected === "Explore"
+              ? styles.sidebarButtonSelected
+              : styles.sidebarButton
+          }
+        >
+          <div>icon</div>
+          <div>Explore</div>
+        </button>
+        <button
+          onClick={() => setSelected("Trips")}
+          className={
+            selected === "Trips"
+              ? styles.sidebarButtonSelected
+              : styles.sidebarButton
+          }
+        >
+          <div>icon</div>
+          <div>Trips</div>
+        </button>
+        <button
+          onClick={() => setSelected("Profile")}
+          className={
+            selected === "Profile"
+              ? styles.sidebarButtonSelected
+              : styles.sidebarButton
+          }
+        >
+          <div>icon</div>
+          <div>Profile</div>
+        </button>
+      </div>
+    </>
+  );
+}
