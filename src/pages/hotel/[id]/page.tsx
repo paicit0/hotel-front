9b40c877d805bc333744daf42846246f15b82ee0
Piyapat <piyapat.pai@outlook.com>
2025-08-31 18:10:16 +0700

    style: add type annotations for useParams and improve fetchHotelById logic

--- src/pages/hotel/[id]/page.tsx
+++ src/pages/hotel/[id]/page.tsx
@@ -2,11 +2,35 @@ import { useParams } from "next/navigation";
 import { useEffect } from "react";
 
 export default function HotelPage() {
-  const { id } = useParams();
+  const params = useParams<{ id: string }>();
+  const id = params.id;
+
+  useEffect(() => {
+    console.log("Location from query params:", location);
+    const url = "http://localhost:8080/getHotelById";
+    const fetchHotelById = async () => {
+      if (!id) {
+        console.error("id is undefined");
+        return;
+      }
+      try {
+        const response = await fetch(
+          `${url}?location=${encodeURIComponent(id)}`
+        );
+        if (!response.ok) {
+          throw new Error(`Response status: ${response.status}`);
+        }
+        const result = await response.json();
+      } catch (error: any) {
+        console.error(error.message);
+      }
+    };
+    fetchHotelById();
+  }, [id]);
 
   useEffect(() => {
     console.log(id);
   }, []);
-  
+
   return <div>Hotel Page</div>;
 }
