3b485ae0f30371e8a8ace6588ca53b827d78ddc7
Piyapat <piyapat.pai@outlook.com>
2025-08-31 18:09:55 +0700

    style: conditionally render Sidebar based on payment route

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Sidebar from "../components/Sidebar";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const isPaymentDoneRoute = router.asPath === "/payment-done";
+
   return (
     <>
-    <Sidebar />
+      {!isPaymentDoneRoute && <Sidebar />}
       <Component {...pageProps} />;
     </>
   );
