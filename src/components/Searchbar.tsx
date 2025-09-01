8cfcedb82b0fdaaf407c3a8bb609419034c6d4fd
Piyapat <piyapat.pai@outlook.com>
2025-09-01 22:03:54 +0700

    feat: add margin props to Searchbar for customizable spacing

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,13 +1,21 @@
 import styles from "../styles/Searchbar.module.css";
-export default function Searchbar() {
+
+type searchbarPropType = {
+  marginLeft?: string;
+  marginRight?: string;
+};
+
+export default function Searchbar({
+  marginLeft = "0px",
+  marginRight = "0px",
+}: searchbarPropType) {
   return (
     <>
-      {/* <form> */}
       <input
         className={styles.searchBar}
+        style={{ marginLeft: marginLeft, marginRight: marginRight }}
         placeholder="Search city, Country, Place for Travel advisory"
       ></input>
-      {/* </form */}
     </>
   );
 }
