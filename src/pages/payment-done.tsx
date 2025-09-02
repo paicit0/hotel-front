00f9ef2b39b045a68cb92218e9e9080b4293c9bc
Piyapat <piyapat.pai@outlook.com>
2025-09-02 21:37:19 +0700

    feat: mobile respon

--- src/pages/payment-done.tsx
+++ src/pages/payment-done.tsx
@@ -9,27 +9,16 @@ export default function PaymentDone() {
     <>
       <div className={styles.container}>
         <img
-          style={{ marginTop: "167px", marginBottom: "48px" }}
+          className={styles.imgDone}
           src="/payment_booking_success_art.png"
           alt="payment_done"
           width={809}
           height={480}
         />
-        <div
-          style={{
-            fontWeight: "600",
-            fontSize: "48px",
-            color: "#2D3DDF",
-            marginBottom: "6px",
-          }}
-        >
+        <div className={styles.bookingCompleteText}>
           Booking Successfully completed
         </div>
-        <div
-          style={{ fontWeight: "300", fontSize: "20px", marginBottom: "49px" }}
-        >
-          Your trip schedule is ready, please check under profile.
-        </div>
+        <div className={styles.scheduleText}>Your trip schedule is ready, please check under profile.</div>
         <button onClick={() => handleHomeClick()} className={styles.homeButton}>
           Home
         </button>
